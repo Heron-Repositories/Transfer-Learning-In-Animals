da49bf28afd8151b47993734cd679f540728431a
georgedimitriadis <gdimitri@hotmail.com>
2022-03-22 13:15:10 +0000

    Fixed bug that stopped the com process over the network to start

--- Sinks/Transfer_Learning/TL_Task2_Screens_Unity/tl_task2_screens_unity_com.py
+++ Sinks/Transfer_Learning/TL_Task2_Screens_Unity/tl_task2_screens_unity_com.py
@@ -5,11 +5,8 @@ import os
 import sys
 from os import path
 
-print(__file__)
 current_dir = path.dirname(path.abspath(__file__))
-print(current_dir)
 while path.split(current_dir)[-1] != r'Heron':
-    #print(current_dir)
     current_dir = path.dirname(current_dir)
 sys.path.insert(0, path.dirname(current_dir))
 
