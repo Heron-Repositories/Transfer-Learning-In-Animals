8d0bc8dc5fdf8d13d92f33079b121502a3942388
georgedimitriadis <gdimitri@hotmail.com>
2023-05-25 17:46:07 +0100

    Added the Unity files for the new scene (discrimination obects)

---
+++ Sinks/Transfer_Learning/TL_Task2_Screens_Unity/__Unity_TL_Task2_Screens_Project/Assets/Scripts/Logic/SpriteControllerForObjectDiscrimination.cs
@@ -0,0 +1,150 @@
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class SpriteControllerForObjectDiscrimination : MonoBehaviour
+{
+    private bool shownOnScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+
+        EventManager.Instance.onUpdatedMotion.AddListener(DoUpdate);
+        EventManager.Instance.onUpdateScreensOn.AddListener(DoScreensSelection);
+        EventManager.Instance.onUpdateTargetTrapOpacity.AddListener(DoShapeOpacityUpdate);
+        EventManager.Instance.onUpdateCueOpacity.AddListener(DoCueOpacityUpdate);
+
+        shownOnScreen = false;
+    }
+
+
+    void DoScreensSelection(string screens)
+    {
+        //Debug.Log(string.Format("Screens for {0} are set to {1}", transform.name, screens));
+        if (transform.name.Contains("Right") && (screens == "Both" || screens == "Right"))
+        {
+            shownOnScreen = true;
+        }
+        if (transform.name.Contains("Front") && (screens == "Both" || screens == "Front"))
+        {
+            shownOnScreen = true;
+        }
+
+        //if (transform.name.Contains("Cue"))
+        //{
+        //    EventManager.Instance.onCueAnimate.Invoke();
+        //}
+    }
+
+    void DoShapeOpacityUpdate(string _opacity)
+    {
+        //Debug.Log(string.Format("Opacity for {0} is set to {1}", transform.name, _opacity));
+        if (transform.name.Contains("Line") || transform.name.Contains("Square") || transform.name.Contains("Circle"))
+        {
+            DoOpacityUpdate(_opacity);
+        }
+    }
+
+    void DoCueOpacityUpdate(string _opacity)
+    {
+        //Debug.Log(_opacity);
+        if (transform.name.Contains("Cue"))
+        {
+            DoOpacityUpdate(_opacity);
+        }
+    }
+
+    void DoOpacityUpdate(string _opacity)
+    {
+        float opacity = float.Parse(_opacity);
+        Color current_color = transform.GetComponent<Image>().color;
+        transform.GetComponent<Image>().color = new Color(current_color.r, current_color.g, current_color.b, opacity);
+    }
+
+    List<float> GetAllSpritesStates(string sprites_message)
+    {
+        string[] sprites_messages = sprites_message.Split(',');
+        List<float> sprites_states = new List<float>();
+        foreach (string sm in sprites_messages)
+        {
+            float state = float.Parse(sm.Substring(sm.IndexOf("=")).Substring(1));
+            sprites_states.Add(state);
+            //Debug.Log(string.Format("{0} || {1}", sm, sprites_states[sprites_states.Count-1]));
+        }
+
+        return sprites_states;
+    }
+
+    float GetStateForThisSprite(List<float> sprites_states)
+    {
+        int sprite_type = new int();
+
+        if (transform.name.Contains("Cue")) sprite_type = 0;
+        if (transform.name.Contains("Line")) sprite_type = 1;
+        if (transform.name.Contains("Circle")) sprite_type = 2;
+        if (transform.name.Contains("Square")) sprite_type = 3;
+
+        return sprites_states[sprite_type];
+    }
+
+    void HideOrShow(float state)
+    {
+        this.gameObject.SetActive(false);
+        //Debug.Log(string.Format("{0} is {1} and screen is {2}", transform.name, state, shownOnScreen));
+        if (state != 0f && shownOnScreen)
+        {
+            this.gameObject.SetActive(true);
+            //Debug.Log(string.Format("{0} is made Active", transform.name));
+        }
+    }
+
+    void DoAnimationIfCue(float state)
+    {
+        //If the sprite is the Cue and it is shown on screen and the state sent by the Heron node is 1 then animate it (that means that states != 1 will not do anything except 0 which will hide it)
+        //Debug.Log(state);
+        if (transform.name.Contains("Cue") && shownOnScreen && state == 1f)
+        {
+           // Debug.Log("Anim invoked");
+            if (transform.name.Contains("Right"))
+                EventManager.Instance.onCueAnimate.Invoke("Right");
+            if (transform.name.Contains("Front"))
+                EventManager.Instance.onCueAnimate.Invoke("Front");
+        }
+    }
+
+    void ChangeTransformIfNotCue(float state)
+    {
+        if (!transform.name.Contains("Cue") && shownOnScreen)
+        {
+            if (transform.name.Contains("Right"))
+            {
+                transform.rotation = Quaternion.Euler(Vector3.forward * 90);
+                int starting_position = 0;
+                transform.localPosition = new Vector3(transform.localPosition.x, starting_position + state, transform.localPosition.z);
+            }
+            else
+            {
+                int starting_position = -972;
+                transform.localPosition = new Vector3(starting_position + state, transform.localPosition.y, transform.localPosition.z);
+            }
+        }
+    }
+
+    void DoUpdate(string sprites_message)
+    {
+        List<float> sprites_states = GetAllSpritesStates(sprites_message);
+
+        float state = GetStateForThisSprite(sprites_states);
+
+        ChangeTransformIfNotCue(state);
+
+        HideOrShow(state);
+
+        DoAnimationIfCue(state);
+
+    }
+}
